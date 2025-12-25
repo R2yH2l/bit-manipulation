9495341183f18a25ceff80cf14a012cf3a34c0ad
R2yH2l <R2yH2l@gmail.com>
2025-12-24 19:39:52 -0600

    Added dump_bin and dump_hex

--- bit-manipulation/main.cpp
+++ bit-manipulation/main.cpp
@@ -1,31 +1,22 @@
 #include <iostream>
-#include <string>
+
+#include "dump.h"
 
 struct alignas(uint32_t) some_struct {
 	uint32_t num_1{};
 	uint32_t num_2{};
+	char c{};
 };
 
-template<typename T>
-static inline std::string bits_to_string(const T& obj) {
-	size_t size_in_bits{ sizeof(T) * 8 };
-	size_t alignment_in_bits{ alignof(T) * 8 };
-
-	std::string str{};
-
-	for (size_t ind{}, grp{}, algn{}; ind < size_in_bits; ind++, grp++, algn++) {
-		if (algn == alignment_in_bits) { algn = 0; str.push_back('\n'); }
-		if (grp == 8) { grp = 0; str.push_back('-'); }
-		str.push_back('0' + (obj >> ((size_in_bits - 1) - ind) & 0x1));
-	}
-
-	return str;
-}
+constexpr utl::field_description some_struct_fields[] = {
+	{ "num_1", offsetof(some_struct, num_1), sizeof(uint32_t) },
+	{ "num_1", offsetof(some_struct, num_2), sizeof(uint32_t) }
+};
 
 int main() {
-	some_struct a{ 0xFF, 0xFF00 };
+	some_struct a{ 0x0F, 0xFF00 };
 
-	std::cout << bits_to_string(a) << std::endl;
+	std::cout << utl::dump_bin(a) << std::endl;
 
 	return 0;
 }
